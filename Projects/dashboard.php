a6ca05f55cc822f911a458438c9d8e7290b0c0c8
mergielynarimuhanan <109118294+mergielynarimuhanan@users.noreply.github.com>
2022-07-23 23:59:57 +0800

    "Modify the background image"

--- Projects/dashboard.php
+++ Projects/dashboard.php
@@ -33,7 +33,7 @@
             <!-- Sidebar - Brand -->
             <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
                 <div class="sidebar-brand-icon rotate-n-15">
-                    <img src="racing.png" width="35px">
+                    <img src="Img1-removebg-preview.png" width="35px">
                   <!--   <i class="fas fa-laugh-wink"></i> -->
                 </div>
                 <div class="sidebar-brand-text mx-3">FAST</div>
